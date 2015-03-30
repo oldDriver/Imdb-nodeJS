1a1de721b1c3ea52b004d4b170775e55c26253aa
oldDriver <fktrctq>
2015-03-30 19:51:00 +0300

    Connected to mysql and create entity User

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Imdb-nodeJS
 1. Setup Express
 2.  npm i yaml-config --save
     npm i swig --save
+3.
+    npm i jugglingdb --save
+    npm i jugglingdb-mysql --save
--- src/frontend/controller/index.js
+++ src/frontend/controller/index.js
@@ -1,3 +1,9 @@
+var User = require('../../../lib/entity/user');
+
 exports.get = function(req, res) {
+    User.all({order: "username", limit: 10}, function(err, results){
+        if (err) throw err;
+        console.log(results);
+    });
     res.render('frontend/index');
 };
