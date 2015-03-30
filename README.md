09d030464797a7d586e8cd70f72cb620bf3c388a
oldDriver <fktrctq>
2015-03-30 20:31:43 +0300

    Entiry in progress

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Imdb-nodeJS
 3.
     npm i jugglingdb --save
     npm i jugglingdb-mysql --save
+
+4.  npm i mongoose --save
+    npm i connect-mongo --save
+    npm i bower --save
+    bower install jquery.datagrid
--- src/frontend/controller/index.js
+++ src/frontend/controller/index.js
@@ -3,7 +3,7 @@ var User = require('../../../lib/entity/user');
 exports.get = function(req, res) {
     User.all({order: "username", limit: 10}, function(err, results){
         if (err) throw err;
-        console.log(results);
+        //console.log(results);
     });
     res.render('frontend/index');
 };
